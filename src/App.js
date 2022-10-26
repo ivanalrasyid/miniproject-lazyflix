54992cf6cb7a0b3b5e6f315aa309e5142b7fd826
ivanalrasyid <ivanalrasyid11@gmail.com>
2022-10-26 14:17:32 +0700

    fix: directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from './components/Header';
+import Header from './components/header/Header.jsx';
 import Home from './pages/home/Home';
 
 function App() {
