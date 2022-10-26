d77c4eaf4cea1be3de431f9b181557b878aa49e7
ivanalrasyid <ivanalrasyid11@gmail.com>
2022-10-26 23:00:37 +0700

    modify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header.jsx';
 import Home from './pages/home/Home';
+import MovieList from './components/movieList/MovieList';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="/movie/:id" element={<h1>Movie Detail Page</h1>}></Route>
-          <Route path="/movies/:type" element={<h1>Movie List Page</h1>}></Route>
+          <Route path="/movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
