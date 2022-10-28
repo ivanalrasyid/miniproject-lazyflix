92525663a904a78a44d03ef68d5d550a3de3d560
ivanalrasyid <ivanalrasyid11@gmail.com>
2022-10-29 04:24:41 +0700

    On Going!

--- src/components/card/Cards.jsx
+++ src/components/card/Cards.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
 import "./Cards.css"
 import { Link } from "react-router-dom"
+import { GlobalContext } from "../../context/GlobalState";
 
 const Cards = ({movie}) => {
   const [isLoading, setIsLoading] = useState(true)
@@ -12,6 +13,23 @@ const Cards = ({movie}) => {
     }, 1500)
   }, [])
 
+  const {
+    addMovieToWatchlist,
+    addMovieToWatched,
+    watchlist,
+    watched,
+  } = useContext(GlobalContext);
+
+  let storedMovie = watchlist.find((o) => o.id === movie.id);
+  let storedMovieWatched = watched.find((o) => o.id === movie.id);
+
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
+
+  const watchedDisabled = storedMovieWatched ? true : false;
 
   return (
     <>
@@ -30,9 +48,26 @@ const Cards = ({movie}) => {
             <div className="cards_overlay">
               <div className="cards_title">{movie?movie.original_title:""}</div>
               <div className="cards_runtime">
-                {movie?movie.relase_date:""}
-                <span className="card_rating">{movie?movie.vote_average:""}<i className="fas fa-start" /></span>
-                <div className="card_description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div>
+                {/* {movie?movie.relase_date:""} */}
+                {/* <span className="card_rating">{movie?movie.vote_average:""}<i className="fas fa-start" /></span> */}
+                {/* <div className="card_description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div> */}
+                <div className="controls">
+                  <button
+                    className="btn btn-watch-list"
+                    disabled={watchlistDisabled}
+                    onClick={() => addMovieToWatchlist(movie)}
+                  >
+                    Add to Watchlist
+                  </button>
+
+                  <button
+                    className="btn btn-watched"
+                    disabled={watchedDisabled}
+                    onClick={() => addMovieToWatched(movie)}
+                  >
+                    Add to Watched
+                  </button>
+                </div>
               </div>
               </div>
             </div>
